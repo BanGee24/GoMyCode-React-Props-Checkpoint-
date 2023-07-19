ec5a349085bbd91796e066842b38685b5cf0a0bb
BanGee24 <bangee99@gmail.com>
2023-07-19 12:12:15 +0100

    React Props

---
+++ src/players.js
@@ -0,0 +1,48 @@
+// the players details
+const players = [
+  {
+    name: "Martin Ødegaard",
+    team: "Arsenal",
+    nationality: "Norway",
+    jerseyNumber: 8,
+    age: 24,
+    //imported the URL link from a website.
+    imageUrl:
+      "https://talksport.com/wp-content/uploads/sites/5/2023/05/martin-odegaard-arsenal-applauds-fans-817473980.jpg",
+  },
+
+  {
+    name: "Erling Haaland",
+    team: "Manchester City",
+    nationality: "Norway",
+    jerseyNumber: 9,
+    age: 22,
+    //imported the URL link from a website.
+    imageUrl:
+      "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/bltf10aa459686f0c90/6449962a2e51ee662f53bd87/1485522329.jpg?auto=webp&format=pjpg&width=3840&quality=60",
+  },
+
+  {
+    name: "Marcus Rashford",
+    team: "Manchester United",
+    nationality: "England",
+    jerseyNumber: 10,
+    age: 25,
+    //imported the URL link from a website.
+    imageUrl:
+      "https://www.telegraph.co.uk/content/dam/football/2023/02/23/TELEMMGLPICT000326040146_trans_NvBQzQNjv4BqpVlberWd9EgFPZtcLiMQf0Rf_Wk3V23H2268P_XkPxc.jpeg",
+  },
+
+  {
+    name: "Vinícius Júnior",
+    team: "Real Madrid",
+    nationality: "Brazil",
+    jerseyNumber: 20,
+    age: 23,
+    //imported the URL link from a website.
+    imageUrl:
+      "https://phantom-marca.unidadeditorial.es/ffdb2cf0dcbf219badea13956f87c8e8/resize/720/f/webp/assets/multimedia/imagenes/2023/07/18/16896831289904.jpg",
+  },
+];
+
+export default players;
